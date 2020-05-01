59aac2ee5ecef0edaf6c8fe4622ad079118ffe0a
Mahmoud Jamal Abu Ghali <abughali@innosoft.me>
2020-05-01 15:02:06 +0200

    create Note data class (model)

---
+++ app/src/main/java/com/ghalym/notesexample/model/Note.kt
@@ -0,0 +1,17 @@
+package com.ghalym.notesexample.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "Note")
+data class Note(
+    @ColumnInfo(name = "id")
+    @PrimaryKey(autoGenerate = true)
+    val id: Long,
+    @ColumnInfo(name = "title")
+    val title: String,
+    @ColumnInfo(name = "content")
+    val content: String
+)
